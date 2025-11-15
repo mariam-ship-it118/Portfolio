f517fd7bd299e0dd7a02805eeb87f31d088834cd
mariam-ship-it118 <22-101170@students.eui.edu.eg>
2025-11-15 15:40:19 +0200

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-
-
+import logo from '../Assets/logo.svg';
+import './Footer.css';
 
 const Footer = () => {
     return ( 
         <>
         
-        
+        <footer class="footer">
+  <div class="footer-top">
+    <h2 class="coloring">Let’s Create Something together.</h2>
+  </div>
+
+  <div class="footer-mid">
+        <img src={logo} alt="visual designer"/>
+
+    <ul class="footer-links">
+      <li>Home</li>
+      <li>Work</li>
+      <li>About</li>
+      <li>Blog</li>
+      <li>Contact</li>
+    </ul>
+    <div class="socials">
+    <li>Behance</li>
+    <li>Dribble</li>
+    <li>LinkedIn</li>
+    <li>Instgram</li>
+    </div>
+
+  </div>
+
+  <div class="footer-bottom">
+    <p class="center wite">© 2025 Maryam Elmarsafy — Visual & UI Designer.</p>
+  </div>
+</footer>
         
         
         
