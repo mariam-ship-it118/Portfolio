02a78ac2350fe95cf7119f9b3d11cb57a3c062ba
mariam-ship-it118 <22-101170@students.eui.edu.eg>
2025-12-02 00:34:35 +0200

    Experience section responsive completed

--- src/Components/ExperienceSection.jsx
+++ src/Components/ExperienceSection.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import StyledText from './TitlesSections';
 import './ExperienceSection.css';
+import './ExperienceSectionRes.css';
 
 const ExperienceSection = (props) => {
     return ( 
